13ea26ead13d4b759d632aabb4df1d67736b9f20
chetan54209 <chetan54209@gmail.com>
2019-10-25 06:53:41 -0400

    forms important methods like set value pathch value and resetting the form

--- src/app/forms/reactive-form/reactive-form.component.ts
+++ src/app/forms/reactive-form/reactive-form.component.ts
@@ -7,7 +7,8 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
-  myReactiveForm:FormGroup
+myReactiveForm:FormGroup
+notAllowedNames = ['chetan']
 genders = [
   {
     id:1,
@@ -22,7 +23,7 @@ genders = [
 
   ngOnInit() {
     this.myReactiveForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
+      'username': new FormControl(null, [Validators.required, this.NaName.bind(this)]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'course': new FormControl('Angular'),
       'gender': new FormControl('Male'),
@@ -31,15 +32,62 @@ genders = [
       ])
 
     });
+    
+    // Some important Methos for async forms
+
+// this.myReactiveForm.valueChanges.subscribe(
+//       (value) => console.log(value))
+
+//   this.myReactiveForm.statusChanges.subscribe(
+//     (status) => console.log(status))
+
+// setTimeout(()=>{
+//   this.myReactiveForm.setValue({
+//     'username': 'csl',
+//     'email': 'abc@abc.com',
+//     'course': 'Html',
+//     'gender': 'Male',
+//     'skills': [
+//       'Photoshop'
+//     ]
+//   })
+// }, 5000);
+
+// this.myReactiveForm.patchValue({
+//   'email': 'abc@abc.com',
+//  })
+
+
   }
 
   onSubmit(){
 console.log(this.myReactiveForm);
+this.myReactiveForm.reset({
+        'username': '',
+        'email': '',
+        'course': 'Angular',
+        'gender': 'Male',
+        'skills': [
+          ''
+        ]
+});
 
   }
+
+  // used for adding the dynamic skill fields from user
 
   onAddSkills(){
     const control = new FormControl (null, Validators.required);
    (<FormArray> this.myReactiveForm.get('skills')).push(control)
   }
+
+  // custom validators
+  // suppose i want that in username fields no on ecan use my username
+NaName(control:FormControl){
+if(this.notAllowedNames.indexOf(control.value)!==-1){
+  return{'nameIsNotAllowed':true}
+     }
+return null; 
+}
+
 }
